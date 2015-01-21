ca313dd064d61b32029b8b0824733b5280bc8e61
Vianney Lecroart <acemtp@gmail.com>
2015-01-21 18:15:34 +0100

    set the teamid to the setting file

--- server/main.js
+++ server/main.js
@@ -1,11 +1,9 @@
 
 isAdmin = function(id) {
   if(isProduction()) {
-    console.log('production server, use the slack admin bool');
     var m = member(id);
     return m && m.slack.is_admin;
   } else {
-    console.log('dev env, everybody are admin');
     return true;
   }
 };
@@ -13,9 +11,9 @@ isAdmin = function(id) {
 Accounts.validateNewUser(function (user) {
   var m = member(user._id);
 
-  if (m && !m.slack.is_restricted && user.profile && user.profile.team_id === 'T029MPGH6')
+  if (m && !m.slack.is_restricted && user.profile && user.profile.team_id === Meteor.settings.slackTeamId)
     return true;
-  throw new Meteor.Error(403, "You must be a member of The Network to log in");
+  throw new Meteor.Error(403, 'You must be a member of the team to log in to Slack');
 });
 
 Accounts.onCreateUser(function(options, user) {
@@ -36,7 +34,7 @@ Accounts.onCreateUser(function(options, user) {
 
 Meteor.startup(function () {
   if(isProduction())
-    console.log('TheNetwork is launched in production mode');
+    console.log('Server is launched in production mode');
   else
     console.log('DEV MODE, everybody are admin');
 });
